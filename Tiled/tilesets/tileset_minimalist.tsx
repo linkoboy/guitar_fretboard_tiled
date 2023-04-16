8e6a510333362371046e536b91ba3975abb0dd8f
Linkoboy <linkoboy78@gmail.com>
2023-04-16 23:10:23 +0200

    test

---
+++ Tiled/tilesets/tileset_minimalist.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="tileset_minimalist" tilewidth="48" tileheight="32" tilecount="620" columns="20">
+ <image source="tileset_minimalist.png" width="1000" height="1000"/>
+</tileset>
