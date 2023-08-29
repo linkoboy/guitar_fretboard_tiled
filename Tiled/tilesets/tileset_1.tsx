97512b4291cbcc6263d70a74d06d265cca8421ec
linkoboy <40902122+linkoboy@users.noreply.github.com>
2023-08-29 14:21:17 +0100

    update

---
+++ Tiled/tilesets/tileset_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_1" tilewidth="48" tileheight="32" tilecount="1085" columns="35" backgroundcolor="#ffffff">
+ <image source="tileset_1.png" width="1687" height="1000"/>
+</tileset>
